cee209cb39d14343bca3475932a528546d90809d
WesleyVitor <wesleydemorais@outlook.com.br>
2024-03-13 20:52:11 -0300

    creating models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import *
+from datetime import datetime
 # Create your models here.
+
+class Author(models.Model):
+    """
+    Guarda informações sobre autores
+    """
+    name = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(null=False, blank=False)
+    description = models.CharField(max_length=400, null=False, blank=False)
+    created_at = models.DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return self.name
